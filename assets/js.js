13453c90693ac01ce0c54651d38138051f556932
Ahmed Abdo <abdelmottale3@gmail.com>
2025-08-09 00:34:34 +0300

    add reusable components by JSON file in main and add footer

---
+++ assets/js.js
@@ -0,0 +1,36 @@
+let cardContainer = document.getElementById("cardContainer")
+
+async function getCategories() {
+    try {
+        const res = await fetch("assets/data/cards.json")
+        const finalRes = await res.json()
+        console.log(finalRes);
+        finalRes.forEach(card => {
+            cardContainer.appendChild(renderCard(card))
+        });
+    } catch (error) {
+        console.log(`error`, error)
+    }
+}
+getCategories()
+
+function renderCard(cardData) {
+    const card = document.createElement("div")
+    card.classList.add("main__cards")
+    card.innerHTML = `
+                <div class="main__cards__card">
+                    <div class="main__cards__card__img display-v">
+                        <img src=${cardData.image} alt="" >
+                        <h6>${cardData.languages}</h6>
+                    </div>
+                    <div class="main__cards__card__description">
+                        <h2>${cardData.title}</h2>
+                        <div class="main__cards__card__button display-h">
+                            <button class="main__button"><a href="">Github</a></button>
+                            <button class="main__button"><a href="">Demo</a></button>
+                        </div>
+                    </div>
+                </div>
+    `
+    return card
+}
